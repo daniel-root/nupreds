ff3c1934cf5c9445c1cfb6a55ff6e2b4479a022b
daniel-root <albuquerquedaniel898@gmail.com>
2020-02-18 14:07:03 -0300

    finalização da primeira implementação

--- 04. IMPLEMENTAÇÃO/CONTEQUI/equipments/urls.py
+++ 04. IMPLEMENTAÇÃO/CONTEQUI/equipments/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
 
-from . import views
+from equipments import views
 
 urlpatterns = [
+    path('', views.equipment_list, name='equipment_list'),
+    path('view/<int:pk>', views.equipment_view, name='equipment_view'),
+    path('new', views.equipment_create, name='equipment_new'),
+    path('edit/<int:pk>', views.equipment_update, name='equipment_edit'),
+    path('delete/<int:pk>', views.equipment_delete, name='equipment_delete'),
+]
+
+'''urlpatterns = [
     path('', views.EquipmentList.as_view(), name='equipment_list'),
     path('view/<int:pk>', views.EquipmentView.as_view(), name='equipment_view'),
     path('new', views.EquipmentCreate.as_view(), name='equipment_new'),
     path('view/<int:pk>', views.EquipmentView.as_view(), name='equipment_view'),
     path('edit/<int:pk>', views.EquipmentUpdate.as_view(), name='equipment_edit'),
     path('delete/<int:pk>', views.EquipmentDelete.as_view(), name='equipment_delete'),
-]
+]'''
--- 04. IMPLEMENTAÇÃO/CONTEQUI/equipments/views.py
+++ 04. IMPLEMENTAÇÃO/CONTEQUI/equipments/views.py
@@ -1,12 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.forms import ModelForm
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
-
 # Create your views here.
 from equipments.models import Equipment
 
+class EquipmentForm(ModelForm):
+    class Meta:
+        model = Equipment
+        fields = ['tag','description','typ','maximum_time']
+
+def equipment_list(request, templete_name='equipments/equipment_list.html'):
+    equipment = Equipment.objects.all()
+    data = {}
+    data['object_list'] = equipment
+    return render(request, templete_name, data)
+
+def equipment_view(request, pk, template_name='equipments/equipment_detail.html'):
+    equipment= get_object_or_404(Equipment, pk=pk)    
+    return render(request, template_name, {'object':equipment})
+
+def equipment_create(request, template_name='equipments/equipment_form.html'):
+    form = EquipmentForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect('equipment_list')
+    return render(request, template_name, {'form':form})
+
+def equipment_update(request, pk, template_name='equipments/equipment_form.html'):
+    equipment= get_object_or_404(Equipment, pk=pk)
+    form = EquipmentForm(request.POST or None, instance=equipment)
+    if form.is_valid():
+        form.save()
+        return redirect('equipment_list')
+    return render(request, template_name, {'form':form})
+
+def equipment_delete(request, pk, template_name='equipments/equipment_confirm_delete.html'):
+    equipment= get_object_or_404(Equipment, pk=pk)    
+    if request.method=='POST':
+        equipment.delete()
+        return redirect('equipment_list')
+    return render(request, template_name, {'object':equipment})
+'''
 class EquipmentList(ListView):
     model = Equipment
 
@@ -26,3 +63,5 @@ class EquipmentUpdate(UpdateView):
 class EquipmentDelete(DeleteView):
     model = Equipment
     success_url = reverse_lazy('equipment_list')
+
+'''
